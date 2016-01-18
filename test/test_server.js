909a97a19ee2490f1f31ade23f0126623ed9e8ab
Natalie Chow <cychownat@gmail.com>
2016-01-18 10:23:21 -0800

    Moved user simulation into separate file

--- test/test_server.js
+++ test/test_server.js
@@ -1,29 +1,9 @@
 const eagles = require(__dirname + '/../index');
 
 var router = new eagles.Router();
-
 router.get('/hello', function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('yooooo');
-  res.end();
-});
-
-router.get('/hello2', function(req, res) {
-  eagles.status(res, 200);
-  eagles.contentType(res, 'plain');
-  eagles.resBody(res, 'looooo');
-  res.end();
-});
-
-router.get('/hello2.5', function(req, res) {
-  eagles.status(res, 200).contentType(res, 'plain');
-  eagles.resBody(res, 'world');
-  res.end();
-});
-
-router.get('/hello3', function(req, res) {
-  eagles.resHead(res, 200, 'plain');
-  eagles.resBody(res, 'polo');
   res.end();
 });
 
